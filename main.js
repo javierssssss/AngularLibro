311097e0dfefc2395bf470cec16182f2fe2e6091
Javier <jalcoser1996@gmail.com>
2021-06-28 20:52:13 -0500

    Cambios

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app,ipcMain, BrowserWindow } = require("electron");
+
+let appWin;
+ipcMain.on("message", (event) => event.reply("reply", "pong"));
+createWindow = () => {
+    appWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Angular and Electron",
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    
+    appWin.loadURL(`file://${__dirname}/dist/index.html`);
+
+    appWin.setMenu(null);
+
+    appWin.webContents.openDevTools();
+
+    appWin.on("closed", () => {
+        appWin = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+});
